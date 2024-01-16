5f6d1802117f055b8573d6e7e94e995c740fe586
Jo Vo <jotanvo@gmail.com>
2024-01-16 14:02:20 +0100

    no message

---
+++ favourite-page-project/js/main.js
@@ -0,0 +1,42 @@
+import products from "./data/products.js";
+console.log("products: ", products);
+// 1. Select product-container
+const productContainer = document.querySelector(".products-container");
+
+// 2. Fill container with products
+
+for (let i = 0; i < products.length; i++ ) {
+    console.log(products[i]);
+    productContainer.innerHTML += `
+            <div class="product">
+                <img src="https://picsum.photos/200" alt="placeholder image">
+                <h3>${products[i].name}</h3>
+                <h4>${products[i].price}</h4>
+                <span class="heart-container">
+                    <svg class="heart active-heart"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" 
+                            fill="none"
+                            stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+                </svg>  
+                </span>
+            </div>
+    `;
+}
+
+// 3. Check if any of the fav btns clicked
+// 4. I want to make this heart to be red
+const favourites = document.querySelectorAll(".heart");
+console.log("favourites: ", favourites)
+
+for(let x = 0; x < favourites.length; x++) {
+        console.log(favourites[x]);
+        favourites[x].addEventListener("click", function () {
+            console.log("Hey you clicked", favourites)
+            this.classList.toggle("active-heart");
+
+        })
+}
+
+//TODO I Want to move MY clicking EvEnT of the Fav buttOn
+// to its own funCtion instead of the anOnYmouS funCtIoN
